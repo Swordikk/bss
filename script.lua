ce1ea64c905d9916a3916ffdb66a3f0fad60e1e1
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-08-30 00:24:47 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -24,7 +24,7 @@ end
 function AutoFarm()
 	while _G.AutoFarm == true do
 		for i,v in pairs(game:GetDescendants()) do
-			if v.Name == "C" then
+			if v.Name == "C" and v.Transparensy = 0 then
 				game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(game:GetService("Workspace").Collectibles.C.Position))
 				game.Players.LocalPlayer.Character.Humanoid.MoveToFinished:Wait()
 			end
