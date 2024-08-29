9a9240a9a132ac11e54609f5a15e34d83beb8034
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-08-29 19:24:49 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -21,11 +21,56 @@ function AutoFarmSnowFlakes()
     end
 end
 
+function AutoFarm()
+	if _G.AutoFarm == "Pepper Patch" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-488.761566, 120.701508, 535.680176, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Rose Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-327.459839, 17.5552464, 129.496735, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Stump Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(424.483276, 94.4255676, -174.810959, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Blue Flower Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(146.865021, 2.13494039, 99.3078308, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Bamboo Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(132.963409, 18.1719551, -25.6000061, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Spider Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-43.4654312, 18.1220875, -13.5899963, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Pine Tree Forest" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-328.670013, 65.5, -187.348999, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Mountain Top Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(77.6849823, 173.500015, -165.431, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Strawberry Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-178.174973, 18.1322384, -9.8549881, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Cactus Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-188.5, 65.5000153, -101.595818, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Coconut Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-254.478104, 68.9707947, 469.459045, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Mushroom Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-89.7000122, 1.95073581, 111.725006, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Pumpkin Patch" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-208.951294, 1.5, 176.579224, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Sunflower Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-208.951294, 1.5, 176.579224, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Clover Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(157.547073, 31.608448, 196.350006, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Pineapple Patch" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(256.498108, 66.1299973, -207.479324, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Dandelion Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-29.6986389, 1.5, 221.572845, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	elseif _G.AutoFarm == "Sunflower Field" then
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-208.951294, 1.5, 176.579224, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	end
+end
+
 function AutoDig()
 	while _G.AutoDig == true do
         game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("ToolCollect"):FireServer()
         task.wait(0.2)
 	end
+end
+
+function AutoSplinker()
+	game.Players.LocalPlayer.Character.Humanoid.Jump = true
+	game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("PlayerActivesCommand"):FireServer(unpack(AutoSplinker))
 end
 
 function AutoClaimAutoWealthClock()
@@ -154,9 +199,6 @@ function FindallStickers()
 end
 
 -- Local Tables  --
-local PanikPas = game:GetService("Workspace").PanikPas
-local Humanoid = game:GetService("Workspace").PanikPas.Humanoid
-
 local DemonMask = {
     [1] = "Equip",
     [2] = {
@@ -224,6 +266,11 @@ local Section = Tab:AddSection({
 	Name = "     Version: v1.0.0"
 })
 
+local AutoSplinker = {
+    [1] = {
+        ["Name"] = "Sprinkler Builder"
+    }
+}
 
 -- Tab Farming --
 local Tab = Window:MakeTab({
@@ -246,7 +293,7 @@ Tab:AddDropdown({
 	Options = {"Pine Tree Forest"},
 	Callback = function(Value)
 		if Value == "Pine Tree Forest" then
-			print("Pine Tree Forest")
+			
 		end
 	end    
 })
@@ -257,6 +304,14 @@ Tab:AddToggle({
 	Callback = function(Value)
 		_G.AutoDig = Value
 		AutoDig()
+	end
+})
+
+Tab:AddToggle({
+	Name = "Auto Splinker",
+	Default = false,
+	Callback = function(Value)
+		
 	end
 })
 
