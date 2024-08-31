82e56ee7182c0e9b4840ca235d6769e03ae7ac1e
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-09-01 02:31:56 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -253,9 +253,13 @@ Tab:AddToggle({
 			if _G.AutoConvert == true then
 				if game:GetService("Players").LocalPlayer.CoreStats.Pollen.Value >= game:GetService("Players").LocalPlayer.CoreStats.Capacity.Value then
 					game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(-338.9832763671875, 68.47433471679688, -72.74992370605469))
+					game.Players.LocalPlayer.Character.Humanoid.MoveToFinished:Wait()
 					game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(-342.8245544433594, 20.424341201782227, 95.5984115600586))
+					game.Players.LocalPlayer.Character.Humanoid.MoveToFinished:Wait()
 					game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(-283.1990966796875, 20.424341201782227, 156.9462127685547))
+					game.Players.LocalPlayer.Character.Humanoid.MoveToFinished:Wait()
 					game.Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(-214.80625915527344, 4.7869415283203125, 288.990173339844))
+					game.Players.LocalPlayer.Character.Humanoid.MoveToFinished:Wait()
 				end
 			end
 		end
