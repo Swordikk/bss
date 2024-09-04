c85eeda73861f0d24c490a6c5ebe743674a1fd76
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-09-04 09:34:41 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -146,7 +146,7 @@ function AutoClaimGingerbreadHouse()
 end
 
 function KillCoconutCrab()
-	if _G.KillCoconutCrab == Value then
+	if _G.KillCoconutCrab == true then
 		local cocopad = Instance.new("Part", game:GetService("Workspace"))
 		cocopad.Name = "Coconut Part"
 		cocopad.Anchored = true
@@ -154,7 +154,7 @@ function KillCoconutCrab()
 		cocopad.Size = Vector3.new(135, 1, 100)
 		cocopad.CanCollide = true
 		cocopad.Position = Vector3.new(-265.52117919922, 105.91863250732, 480.86791992188)
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-254.646957, 109.892967, 448.79892, -0.998998344, -2.36307951e-08, 0.0447468422, -2.06483168e-08, 1, 6.71144704e-08, -0.0447468422, 6.61232988e-08, -0.998998344)
+		HumanoidRootPart.CFrame = CFrame.new(-254.646957, 109.892967, 448.79892, -0.998998344, -2.36307951e-08, 0.0447468422, -2.06483168e-08, 1, 6.71144704e-08, -0.0447468422, 6.61232988e-08, -0.998998344)
 		--[[for i,v in pairs(game:GetService("Workspace").Collectibles:GetChildren()) do
 			                if (v.Position-HumanoidRootPart.Position).Magnitude < 50 and v.BackDecal.Texture == "rbxassetid://1629547638" or v.BackDecal.Texture == "rbxassetid://2319083910"
 						or v.BackDecal.Texture == "http://www.roblox.com/asset/?id=253828517" then
