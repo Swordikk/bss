cf2a2cd519f2ae96c61d087b11e7b3de3c1584b1
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-09-07 23:25:10 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -906,6 +906,32 @@ local Tab = Window:MakeTab({
 })
 
 Tab:AddTextbox({
+	Name = "Anti-AFK",
+	Default = "",
+	TextDisappear = false,
+	Callback = function(Value)
+		if Value == true then
+			while not game:IsLoaded() do wait() end
+			repeat wait() until game.Players.LocalPlayer.Character
+			Players = game:GetService("Players")
+			local GC = getconnections or get_signal_cons
+			if GC then
+				for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
+					if v["Disable"] then v["Disable"](v)
+					elseif v["Disconnect"] then v["Disconnect"](v)
+					end
+				end
+			else
+			Players.LocalPlayer.Idled:Connect(function()
+				VirtualUser:CaptureController()
+				VirtualUser:ClickButton2(Vector2.new())
+  				end)
+			end
+		end
+	end
+})
+
+Tab:AddTextbox({
 	Name = "WalkSpeed",
 	Default = "",
 	TextDisappear = false,
